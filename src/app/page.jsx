4fd123efc3c99d9104ad525d87132b4a5d91ab9c
omargamal510 <34138436+omargamal510@users.noreply.github.com>
2024-08-29 00:03:49 +0300

    .

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default async function Home() {
+  // const res = await fetch("https://fakestoreapi.com/products");
+  // const data = await res.json();
+
+  return <></>;
+}
